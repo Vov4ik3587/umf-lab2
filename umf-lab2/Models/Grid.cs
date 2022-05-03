4c318ed01d205845f72d65982fe4ef0bb4fe4739
Vov4ik3587 <galstyan.vova@gmail.com>
2022-05-03 12:49:16 +0700

    переделал сетку через более быстрые массивы

--- umf-lab2/Models/Grid.cs
+++ umf-lab2/Models/Grid.cs
@@ -5,39 +5,43 @@
 /// </summary>
 public class Grid
 {
-    public List<double> LocalNumberNode = new List<double>();
-    public List<double> XValueNode = new List<double>();
+    public double[] LocalNumberNode;
+    public double[] XValueNode;
     public readonly int AmountOfElements;
 
     public Grid(Area area)
     {
         AmountOfElements = area.AmountNodes - 1;
+
+        LocalNumberNode = new double[area.AmountNodes];
+        XValueNode = new double[area.AmountNodes];
         
         var lengthOfArea = area.RightBorder - area.LeftBorder;
         var initStep = lengthOfArea / AmountOfElements;
         var xCurrentDot = area.LeftBorder;
         
-        //Вынес первую итерацию цикла for 
-        LocalNumberNode.Add(1);
-        XValueNode.Add(xCurrentDot);
+        //Вынес первую итерацию цикла for
+        LocalNumberNode[0] = 1;
+        XValueNode[0] = area.LeftBorder;
         xCurrentDot = xCurrentDot + initStep;
         
         
         for (int i = 1; i < AmountOfElements ; i++)
         {
             var step = initStep * area.DischargeCoefficient;
-            LocalNumberNode.Add(i+1);
-            XValueNode.Add(xCurrentDot);
+            LocalNumberNode[i] = i + 1;
+            XValueNode[i] = xCurrentDot;
             xCurrentDot += step;
         }
-        XValueNode.Add(area.RightBorder);
-        LocalNumberNode.Add(area.AmountNodes);
+
+        XValueNode[area.AmountNodes - 1] = area.RightBorder;
+        LocalNumberNode[area.AmountNodes - 1] = area.AmountNodes;
     }
 
     public void PrintGrid()
     {
         Console.WriteLine("Список локальных номеров сетки:");
-        foreach (var item in this.LocalNumberNode)
+        foreach (var item in LocalNumberNode)
         {
             Console.WriteLine(item);
         }
